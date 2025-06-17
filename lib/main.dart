a26cec69d253c0fbccfe5f9bb2e0e506a5134a8e
Hoang Tuan Hiep <hiephoang1@cqtdt.aisa>
2025-06-17 16:35:02 +0700

    use notifier to change brightness mode and bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:quizflutter/components/AppRouting.dart';
 import 'package:quizflutter/constants/app_routes.dart';
-import 'package:quizflutter/views/auth/login_screen.dart';
-import 'package:quizflutter/views/quiz/quiz_list_screen.dart';
-import 'package:quizflutter/views/websocket/chat_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:quizflutter/utility/notifier.dart';
+import 'package:quizflutter/views/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HoangHiep',
-      initialRoute: AppRoutes.login,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
-        useMaterial3: true,
-      ),
-      home: const Home(title: 'Welcome'),
-    );
-  }
-}
-class Home extends StatefulWidget {
-  final String title;
-  const Home({super.key, required this.title});
-  @override
-  State<StatefulWidget> createState() {
-    return HomeState();
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
-        title: Text(title),
-        titleTextStyle: const TextStyle(
-          fontSize: 24,          // Set font size
-          fontWeight: FontWeight.bold, // Set font weight
-        ),
-      )
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class HomeState extends State<Home> {
-  
-  int selectedIdx = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  late String username = '';
-
-  Future<void> loadUsername() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String? email = prefs.getString("email");
-    if (email != null) {
-      setState(() {
-        username = email;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    loadUsername();
-  } // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: Text('Hi, $username'),
-            leading: Builder(builder: (context) {
-              return IconButton(
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  icon: const Icon(Icons.menu));
-            })),
-        drawer: Drawer(
-            child: ListView(padding: EdgeInsets.zero, children: [
-          DrawerHeader(
-              decoration: const BoxDecoration(color: Colors.white),
-              child: Text('Hi, $username')),
-          ListTile(
-              title: const Text('Quiz'),
-              selected: selectedIdx == 0,
-              onTap: () {
-                if (username.isNotEmpty) {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const QuizListScreen()));
-                } else {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const RoutingApp()));
-                }
-              }),
-          ListTile(
-              title: const Text('Chat'),
-              selected: selectedIdx == 1,
-              onTap: () {
-                if (username.isNotEmpty) {
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (context) =>
-                          ChatPage(title: "Chatting Platform", username: username,)));
-                } else {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const RoutingApp()));
-                }
-              })
-        ])),
-        body: Center(child: Text('Welcome $username to Quiz Flutter')),
-    );
+    return ValueListenableBuilder(
+        valueListenable: isDarkModeNotifier,
+        builder: (context, isDarkMode, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'HoangHiep',
+            initialRoute: AppRoutes.login,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.deepPurpleAccent,
+                  brightness: isDarkMode ? Brightness.dark : Brightness.light),
+              useMaterial3: true,
+            ),
+            home: const Home(title: 'Welcome'),
+          );
+        });
   }
 }
---
+++ lib/views/home.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:quizflutter/components/AppRouting.dart';
+import 'package:quizflutter/components/navbar_widget.dart';
+import 'package:quizflutter/utility/notifier.dart';
+import 'package:quizflutter/views/profile.dart';
+import 'package:quizflutter/views/quiz/quiz_list_screen.dart';
+import 'package:quizflutter/views/websocket/chat_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+List<Widget> pages = [const ProfilePage(), const ProfilePage()];
+
+class Home extends StatefulWidget {
+  final String title;
+  const Home({super.key, required this.title});
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        title: Text(title),
+        titleTextStyle: const TextStyle(
+          fontSize: 24,          // Set font size
+          fontWeight: FontWeight.bold, // Set font weight
+        ),
+        leading: const Icon(Icons.notification_add, color: Colors.amber,),
+      ),
+
+      floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(onPressed: () {print ('Floating Action Button');}, child: const Icon(Icons.arrow_upward)),
+            const SizedBox(height: 10.0,)
+          ]
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+        ],
+        onDestinationSelected: (int value) {},
+        selectedIndex: 0,
+      ),
+    );
+  }
+}
+
+class HomeState extends State<Home> {
+
+  int selectedIdx = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  late String username = '';
+
+  Future<void> loadUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? email = prefs.getString("email");
+    if (email != null) {
+      setState(() {
+        username = email;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUsername();
+  } // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Hi, $username'),
+          leading: Builder(builder: (context) {
+            return IconButton(
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: const Icon(Icons.menu));
+          }),
+        actions: [
+          IconButton(onPressed: () {isDarkModeNotifier.value = !isDarkModeNotifier.value;},
+              icon: ValueListenableBuilder(
+                  valueListenable: isDarkModeNotifier,
+                  builder: (context, isDarkMode, child) {
+                    return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode);
+                  }))
+        ],
+      ),
+      drawer: Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: [
+        DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.deepPurpleAccent),
+            child: Text('Hi, $username')
+        ),
+        ListTile(
+            leading: const Icon(Icons.join_full),
+            title: const Text('Quiz'),
+            selected: selectedIdx == 0,
+            onTap: () {
+              if (username.isNotEmpty) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const QuizListScreen()));
+              } else {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RoutingApp()));
+              }
+            }),
+        ListTile(
+          title: const Text('Chat'),
+          selected: selectedIdx == 1,
+          onTap: () {
+            if (username.isNotEmpty) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ChatPage(
+                            title: "Chatting Platform",
+                            username: username,
+                          )));
+            } else {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const RoutingApp()));
+            }
+          },
+          trailing: const Text('realtime'),
+        )
+      ])),
+      body: Column(
+        children: [
+          Center(child: Text('Welcome $username to Quiz Flutter')),
+          ValueListenableBuilder(
+              valueListenable: selectedPageNotifier,
+              builder: (context, selectedPage, child) {
+                return pages.elementAt(selectedPage);
+              }),
+        ],
+      ),
+      bottomNavigationBar: const NavbarWidget(),
+    );
+  }
+}
